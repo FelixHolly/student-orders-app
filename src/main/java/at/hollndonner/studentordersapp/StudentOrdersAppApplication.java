54e38cb08b0eb4aafece8400a608dab0362feb5b
Felix <felixhollndonner@hotmail.com>
2025-12-05 20:39:44 -0700

    init

---
+++ src/main/java/at/hollndonner/studentordersapp/StudentOrdersAppApplication.java
@@ -0,0 +1,13 @@
+package at.hollndonner.studentordersapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentOrdersAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentOrdersAppApplication.class, args);
+    }
+
+}
